bed422f80d9cb5f979906e964093c3c47314ba91
Alisha <alisha27011997@gmail.com>
2019-11-14 15:51:05 -0500

    Add GPA file

--- 765497_Alisha_gpaApp/ViewController.swift
+++ 765497_Alisha_gpaApp/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var FirstNameTF: UITextField!
+    @IBOutlet weak var LastNameTF: UITextField!
+    @IBOutlet weak var IDTextField: UITextField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func SaveButton(_ sender: UIButton)
+    {
+        var firstName = FirstNameTF.text
+        var lastName = LastNameTF.text
+        var studentID = IDTextField.text
+        
+        let student = Student(Firstname: firstName!, Lastname: lastName!, StuID: studentID!)
+        
+        Student.Students.append(student)
+        
+        let alertController = UIAlertController(title: "Are you Sure", message: "", preferredStyle: <#T##UIAlertController.Style#>)
+        
+        
+        
+    }
+    
 }
 
